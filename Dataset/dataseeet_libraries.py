635dee35b3aafd9d21c077799e6011677229641c
Pan Rico <juanjulianpaniagua@gmail.com>
2024-06-25 11:13:46 -0600

    Tercer Commit

--- Dataset/dataseeet_libraries.py
+++ Dataset/dataseeet_libraries.py
@@ -1,11 +1,24 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from sklearn.impute import SimpleImputer #Impute sirve para valores vacios usando media,promedio,etc
 dataset = pd.read_csv("Dataset\Online Sales Data.csv")
-
-x = dataset.iloc[0].iloc[0]
+x = dataset.iloc[:,:-2]
 y = dataset.iloc[:,0].values
 
-print(x)    
+
+
+
+
+listaTest = dataset.iloc[:,4:7]
+
+imputer = SimpleImputer(missing_values=np.nan,strategy="most_frequent")
+
+imputer.fit(listaTest)
+listaImpute = imputer.transform(listaTest.copy())
+
+print(listaTest)
+print(listaImpute)
+
+
 #print(dataset)
